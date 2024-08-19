9795246fb1e427206104c244ee0885a897f5238a
어지민 <djwlalssla@naver.com>
2024-08-19 14:37:58 +0900

    feat: sideBar 배경 삽입

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ let isSideBarOpen = false;
 
 const sideBar = document.querySelector(".side-bar");
 const sideOpen = document.querySelector(".side-open");
+const sideBarBack = document.querySelector(".side-bar-back");
 
 sideOpen.addEventListener("click", (element) => {
   console.log(isSideBarOpen);
   if (isSideBarOpen) {
-    sideBar.classList.add("none");
+    sideBar.classList.remove("open");
+    sideBarBack.classList.add("close");
     isSideBarOpen = false;
   } else {
-    sideBar.classList.remove("none");
+    sideBar.classList.add("open");
+    sideBarBack.classList.remove("close");
     isSideBarOpen = true;
+  }
+});
+
+sideBarBack.addEventListener("click", () => {
+  console.log("화면 클릭");
+  if (sideOpen) {
+    sideBar.classList.remove("open");
+    sideBarBack.classList.add("close");
+    isSideBarOpen = false;
   }
 });
